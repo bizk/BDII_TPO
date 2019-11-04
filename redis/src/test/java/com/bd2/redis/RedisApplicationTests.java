a5a91a8afe029475c9b0703f31d8871ac8b33b2a
bizk <santiago.yanzon1999@gmail.com>
2019-11-04 19:46:28 -0300

    Projects init

---
+++ redis/src/test/java/com/bd2/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bd2.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
